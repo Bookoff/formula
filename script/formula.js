db48763a1ccc41e6b011e58fa0efad4a00158780
Wiliam Souza <wiliamsouza83@gmail.com>
2014-03-14 16:40:41 -0300

    Added basic car movement and wall

--- script/formula.js
+++ script/formula.js
@@ -1,15 +1,68 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game',
-    { preload: preload, create: create, update: update });
+var game = new Phaser.Game(400, 600, Phaser.AUTO, 'game', {preload: preload, create: create, update: update});
 
 function preload() {
     game.load.image('car', 'assets/car.png');
+    game.load.image('stone', 'assets/stone.png');
 }
 
+var car;
+var stones;
+var cursors;
+var time;
+
 function create() {
-    this.car_sprite = game.add.sprite(400, 400, 'car');
-    this.car_sprite.body.velocity.y=-50;
+    car = game.add.sprite(187, 450, 'car');
+    car.body.collideWorldBounds = true;
+
+    // The stones group contains the obstacles the car should avoid
+    stones = game.add.group();
+    stones.createMultiple(24, 'stone');
+    stones.setAll('outOfBoundsKill', true);
+
+    // Keyboard controls
+    cursors = game.input.keyboard.createCursorKeys();
+
+    // Add stones to the game every 2 seconds
+    timer = game.time.events.loop(2000, add_stones, this);
 }
 
 function update() {
-    this.car_sprite.angle += 1;
+
+    // Reset car velocity movement
+    car.body.velocity.x = 0;
+
+    // Collide the car with the stones
+    game.physics.collide(car, stones);
+
+    //  Move to the left
+    if (cursors.left.isDown)
+    {
+        car.body.velocity.x = -300;
+    }
+
+    //  Move to the right
+    if (cursors.right.isDown)
+    {
+        car.body.velocity.x = 300;
+    }
+}
+
+function add_stone(x, y){
+    var stone = stones.getFirstDead();
+    stone.reset(x, y);
+    stone.body.velocity.y = 200;
+}
+
+function add_stones() {
+    var hole = Math.floor(Math.random()*8);
+    for (var i = 0; i < 8; i++)
+        if (i != hole) 
+            add_stone(i*50, 0);  
+}
+
+function restart() {
+    // Stop the timer
+    game.time.events.remove(timer);
+
+    game.state.start();
 }
